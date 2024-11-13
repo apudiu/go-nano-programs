d5d88c7e873f77a1a3276a359d897f1b8098fab9
Mohammad A. Apu <apuatcfbd@gmail.com>
2024-11-13 20:59:53 +0600

    Moved helper fn inside

--- find-memory-usage/main.go
+++ find-memory-usage/main.go
@@ -5,12 +5,15 @@ import (
 	"runtime"
 )
 
-func printMemoryUsage() uint64 {
+func printMemoryUsage() {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
 
+	b2Mb := func(b uint64) uint64 {
+		return b / 1000 / 1000
+	}
+
 	log.Printf("Heap: %d mb, Total(so far): %d mb, Heap+Stack: %d mb \n", b2Mb(m.Alloc), b2Mb(m.TotalAlloc), b2Mb(m.Sys))
-	return m.Alloc
 }
 
 func main() {
@@ -24,8 +27,4 @@ func main() {
 
 	log.Println("Memory usage after")
 	printMemoryUsage()
-}
-
-func b2Mb(b uint64) uint64 {
-	return b / 1000 / 1000
 }
