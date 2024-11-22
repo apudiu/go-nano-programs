f33ed50f88f6565f25d14ee3b4a88a40cb4fb19f
Mohammad A. Apu <apuatcfbd@gmail.com>
2024-11-22 18:30:57 +0600

    more comment added

--- mistakes/2-functional-options-pattern/Builder pattern/server.go
+++ mistakes/2-functional-options-pattern/Builder pattern/server.go
@@ -1,47 +1,47 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 type Config struct {
-    Port int
+	Port int
 }
 
 type ConfigBuilder struct {
-    port *int
+	port *int
 }
 
 func (c *ConfigBuilder) Port(p int) *ConfigBuilder {
-    c.port = &p
-    return c
+	c.port = &p
+	return c
 }
 
 func (c *ConfigBuilder) Build() (Config, error) {
-    cfg := Config{}
-    if c.port == nil { // when port is not provided use default
-        defaultPort := 3000
-        cfg.Port = defaultPort
-    } else {
-        if *c.port == 0 { // when port is 0 use random port
-            randomPort := 4000
-            cfg.Port = randomPort
-        } else if *c.port < 0 { // validate passed port
-            return cfg, fmt.Errorf("port %d must be positive", *c.port)
-        } else { // when provided port is okay, use that
-            cfg.Port = *c.port
-        }
-    }
+	cfg := Config{}
+	if c.port == nil { // when port is not provided use default
+		defaultPort := 3000
+		cfg.Port = defaultPort
+	} else {
+		if *c.port == 0 { // when port is 0 use random port
+			randomPort := 4000
+			cfg.Port = randomPort
+		} else if *c.port < 0 { // validate passed port
+			return cfg, fmt.Errorf("port %d must be positive", *c.port)
+		} else { // when provided port is okay, use that
+			cfg.Port = *c.port
+		}
+	}
 
-    return cfg, nil
+	return cfg, nil
 }
 
 func NewServer(addr string, cfg Config) (*http.Server, error) {
-    return &http.Server{
-        Addr:    fmt.Sprintf("%s:%d", addr, cfg.Port),
-        Handler: nil,
-    }, nil
+	return &http.Server{
+		Addr:    fmt.Sprintf("%s:%d", addr, cfg.Port),
+		Handler: nil,
+	}, nil
 }
 
 // problems with this approach
