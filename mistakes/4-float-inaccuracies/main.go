a5bd854a99e666ae09fae7fe5b74ffca0279615f
Mohammad A. Apu <apuatcfbd@gmail.com>
2024-11-23 16:54:28 +0600

    Added float example

--- mistakes/4-float-inaccuracies/main.go
+++ mistakes/4-float-inaccuracies/main.go
@@ -3,24 +3,24 @@ package main
 import "fmt"
 
 func main() {
-    n := 500
+	n := 500
 
-    fmt.Printf("F1: %#v \n", f1(n))
-    fmt.Printf("F2: %#v \n", f2(n))
+	fmt.Printf("F1: %#v \n", f1(n))
+	fmt.Printf("F2: %#v \n", f2(n))
 }
 
 func f1(n int) float64 {
-    result := 10_000.
-    for i := 0; i < n; i++ {
-        result += 1.0001
-    }
-    return result
+	result := 10_000.
+	for i := 0; i < n; i++ {
+		result += 1.0001
+	}
+	return result
 }
 
 func f2(n int) float64 {
-    result := 0.
-    for i := 0; i < n; i++ {
-        result += 1.0001
-    }
-    return result + 10_000.
+	result := 0.
+	for i := 0; i < n; i++ {
+		result += 1.0001
+	}
+	return result + 10_000.
 }
