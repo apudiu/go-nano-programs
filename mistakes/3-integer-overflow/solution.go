6919cf33c7a546b1d11ff5460f42a795a310f1f5
Mohammad A. Apu <apuatcfbd@gmail.com>
2024-11-23 15:27:16 +0600

    test commit

--- mistakes/3-integer-overflow/solution.go
+++ mistakes/3-integer-overflow/solution.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-    "fmt"
-    "math"
+	"fmt"
+	"math"
 )
 
 func main() {
-    var n int8 = math.MaxInt8
-    fmt.Printf("%#v \n", n) // this a signed int
-    increment(n)            // fails here when overflowing
-    fmt.Printf("%#v \n", n) // it became unsigned & this is not detected at compile time
+	var n int8 = math.MaxInt8
+	fmt.Printf("%#v \n", n) // this a signed int
+	increment(n)            // fails here when overflowing
+	fmt.Printf("%#v \n", n) // it became unsigned & this is not detected at compile time
 }
 
 func increment(n int8) int8 {
-    if n == math.MaxInt8 {
-        panic("Int8 overflow!")
-    } // sdf
-    return n + 1
+	if n == math.MaxInt8 {
+		panic("Int8 overflow!")
+	}
+	return n + 1
 }
