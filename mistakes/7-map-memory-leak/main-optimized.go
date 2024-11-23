85b11d758d7233ba76317a9eb75fa035adf68476
Mohammad A. Apu <apuatcfbd@gmail.com>
2024-11-23 21:01:24 +0600

    Added map optimized example

--- mistakes/7-map-memory-leak/main-optimized.go
+++ mistakes/7-map-memory-leak/main-optimized.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-    "fmt"
-    "runtime"
+	"fmt"
+	"runtime"
 )
 
 func main() {
-    n := 1_000_000
-    m := make(map[int]*[128]byte)
-    printAlloc2()
+	n := 1_000_000
+	m := make(map[int]*[128]byte)
+	printAlloc2()
 
-    for i := 0; i < n; i++ {
-        m[i] = randBytes2()
-    }
-    printAlloc2()
+	for i := 0; i < n; i++ {
+		m[i] = randBytes2()
+	}
+	printAlloc2()
 
-    for i := 0; i < n; i++ {
-        delete(m, i)
-    }
+	for i := 0; i < n; i++ {
+		delete(m, i)
+	}
 
-    runtime.GC()
+	runtime.GC()
 
-    // NOTICE: memory is not fully reduced, indicates memory leak
-    printAlloc2()
+	// NOTICE: memory is not fully reduced, indicates memory leak
+	printAlloc2()
 
-    runtime.KeepAlive(m)
+	runtime.KeepAlive(m)
 }
 
 func randBytes2() *[128]byte {
-    return &[128]byte{}
+	return &[128]byte{}
 }
 
 func printAlloc2() {
-    var m runtime.MemStats
-    runtime.ReadMemStats(&m)
-    fmt.Printf("%d MB\n", m.Alloc/1024/1024)
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Printf("%d MB\n", m.Alloc/1024/1024)
 }
