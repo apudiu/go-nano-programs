6cdc6dde8517bfcf334889eb09890a82bca4bc11
Mohammad A. Apu <apuatcfbd@gmail.com>
2024-11-28 21:45:29 +0600

    Added range copy examples

--- mistakes/8-range-copy/arr-copy-by-raneg-loop.go
+++ mistakes/8-range-copy/arr-copy-by-raneg-loop.go
@@ -4,28 +4,28 @@ import "fmt"
 
 func main() {
 
-    a := [3]int{1, 2, 3}
+	a := [3]int{1, 2, 3}
 
-    for i, v := range a {
+	for i, v := range a {
 
-        // NOTICE: this isn't printed as the updated array (a) is copied before loop start
-        a[2] = 10
+		// NOTICE: this isn't printed as the updated array (a) is copied before loop start
+		a[2] = 10
 
-        if i == 2 {
-            fmt.Printf("-> %#v \n", v)
-        }
-    }
+		if i == 2 {
+			fmt.Printf("-> %#v \n", v)
+		}
+	}
 
-    fmt.Printf("%#v \n", "----------------------------------")
+	fmt.Printf("%#v \n", "----------------------------------")
 
-    for i, v := range &a {
+	for i, v := range &a {
 
-        // NOTICE: this is printed as the updated array (a) because range is copying the address not the value
-        a[2] = 10
+		// NOTICE: this is printed as the updated array (a) because range is copying the address not the value
+		a[2] = 10
 
-        if i == 2 {
-            fmt.Printf("-> %#v \n", v)
-        }
-    }
+		if i == 2 {
+			fmt.Printf("-> %#v \n", v)
+		}
+	}
 
 }
