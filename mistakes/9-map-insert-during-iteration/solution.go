e44b30bf06bed0bdbc1405653b4f6bf9c3452777
Mohammad A. Apu <apuatcfbd@gmail.com>
2024-11-29 20:08:24 +0600

    Added maps example

--- mistakes/9-map-insert-during-iteration/solution.go
+++ mistakes/9-map-insert-during-iteration/solution.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-    "fmt"
-    "maps"
+	"fmt"
+	"maps"
 )
 
 func main() {
-    m := map[int]bool{
-        1: true,
-        2: false,
-        3: true,
-    }
+	m := map[int]bool{
+		1: true,
+		2: false,
+		3: true,
+	}
 
-    m2 := maps.Clone(m)
+	m2 := maps.Clone(m)
 
-    for i, v := range m {
-        if v { // insert in loop
-            m2[i+10] = true
-        }
-    }
+	for i, v := range m {
+		if v { // insert in loop
+			m2[i+10] = true
+		}
+	}
 
-    fmt.Printf("[ PREDICTABLE ]: %#v \n", m2)
+	fmt.Printf("[ PREDICTABLE ]: %#v \n", m2)
 }
