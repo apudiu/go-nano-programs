52988f0aab9ff3f65fbfdbc1fdcfbef9d524f6b3
Mohammad A. Apu <apuatcfbd@gmail.com>
2024-11-29 21:14:00 +0600

    comment added

--- mistakes/10-inaccurate-string-iteration/solution.go
+++ mistakes/10-inaccurate-string-iteration/solution.go
@@ -3,11 +3,17 @@ package main
 import "fmt"
 
 func main() {
-    s := "hêllo"
-    fmt.Printf("The str is: %s \n", s)
+	s := "hêllo"
+	fmt.Printf("The str is: %s \n", s)
 
-    for i, v := range s {
-        // NOTICE: here we taken whole rune, so it doesn't matter how many bytes it has
-        fmt.Printf("position %d: %c\n", i, v)
-    }
+	for i, v := range s {
+		// NOTICE: here we taken whole rune, so it doesn't matter how many bytes it has
+		fmt.Printf("position %d: %c\n", i, v)
+	}
 }
+
+// The other approach is to convert the string into a slice of runes and iterate over it:
+// like:
+//s := "hêllo"
+//runes := []rune(s)
+// range over rules
