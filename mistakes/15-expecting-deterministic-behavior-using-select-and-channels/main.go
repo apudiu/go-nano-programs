d3a56f0bcfea73f8ae8328257d4b8cceaf38f71d
Mohammad A. Apu <74357797+apuatcfbd@users.noreply.github.com>
2024-12-28 21:13:05 +0600

    Added channel merge example

--- mistakes/15-expecting-deterministic-behavior-using-select-and-channels/main.go
+++ mistakes/15-expecting-deterministic-behavior-using-select-and-channels/main.go
@@ -4,53 +4,79 @@ import "fmt"
 
 func main() {
 
-	msgChan := make(chan string, 4) // this can be guaranteed with 'noMsgDeliveryGuarantee' when unbuffered
-	disconnectChan := make(chan string)
-	defer close(msgChan)
-	defer close(disconnectChan)
+    msgChan := make(chan string, 4) // this can be guaranteed with 'noMsgDeliveryGuarantee' when unbuffered
+    disconnectChan := make(chan string)
+    defer close(msgChan)
+    defer close(disconnectChan)
 
-	go func() {
-		//noMsgDeliveryGuarantee(msgChan, disconnectChan)
-		msgDeliveryGuarantee(msgChan, disconnectChan)
-	}()
+    go func() {
+        //noMsgDeliveryGuarantee(msgChan, disconnectChan)
+        msgDeliveryGuarantee(msgChan, disconnectChan)
+    }()
 
-	for i := 0; i < 10; i++ {
-		msgChan <- fmt.Sprintf("Message #%d", i)
-	}
+    for i := 0; i < 10; i++ {
+        msgChan <- fmt.Sprintf("Message #%d", i)
+    }
 
-	disconnectChan <- ""
+    disconnectChan <- ""
 }
 
 func noMsgDeliveryGuarantee(msgChan <-chan string, disconnCh <-chan string) {
-	for {
-		select {
-		case msg := <-msgChan:
-			fmt.Printf("%#v \n", msg)
-		case <-disconnCh:
-			fmt.Println("disconnected! channel closed")
-			return
-		}
-	}
+    for {
+        select {
+        case msg := <-msgChan:
+            fmt.Printf("%#v \n", msg)
+        case <-disconnCh:
+            fmt.Println("disconnected! channel closed")
+            return
+        }
+    }
 }
 
 func msgDeliveryGuarantee(msgChan <-chan string, disconnCh <-chan string) {
-	for {
-		select {
-		case msg := <-msgChan:
-			fmt.Printf("%#v \n", msg)
-		case <-disconnCh:
+    for {
+        select {
+        case msg := <-msgChan:
+            fmt.Printf("%#v \n", msg)
+        case <-disconnCh:
 
-			// when disconnecting, check for remaining messages
-			for {
-				select {
-				case msg := <-msgChan:
-					fmt.Printf("-> %#v \n", msg)
-				default:
-					fmt.Println("disconnected! channel closed")
-					return
-				}
-			}
+            // when disconnecting, check for remaining messages
+            for {
+                select {
+                case msg := <-msgChan:
+                    fmt.Printf("-> %#v \n", msg)
+                default:
+                    fmt.Println("disconnected! channel closed")
+                    return
+                }
+            }
 
-		}
-	}
+        }
+    }
+}
+
+func mergeChan(ch1, ch2 <-chan int) <-chan int {
+    ch := make(chan int, 1)
+
+    go func() {
+        for ch1 != nil || ch2 != nil {
+            select {
+            case v, open := <-ch1:
+                if !open {
+                    ch1 = nil // a nil channel is not evaluated in select
+                    break
+                }
+                ch <- v
+            case v, open := <-ch2:
+                if !open {
+                    ch2 = nil
+                    break
+                }
+                ch <- v
+            }
+        }
+        close(ch)
+    }()
+
+    return ch
 }
