997cb8c1d521214a37d1f37eeb07ffa42db9ec6e
Mohammad A. Apu <74357797+apuatcfbd@users.noreply.github.com>
2025-04-09 21:12:20 +0600

    Added allocation optimization ways

--- mistakes/19-stack-vs-heap/main.go
+++ mistakes/19-stack-vs-heap/main.go
@@ -2,10 +2,10 @@ package main
 
 func main() {
 
-	ch := make(chan Foo, 1)
-	s := "x" // escapes to heap as points by Foo
-	bar := Foo{s: &s}
-	ch <- bar
+    ch := make(chan Foo, 1)
+    s := "x" // escapes to heap as points by Foo
+    bar := Foo{s: &s}
+    ch <- bar
 }
 
 type Foo struct{ s *string }
@@ -18,3 +18,10 @@ type Foo struct{ s *string }
 // 3. If the backing array of a slice is reallocated using append.
 
 // In general, sharing down (in stack frames) stays on the stack, whereas sharing up escapes to the heap
+
+// Ways to reduce extra allocations
+//1. Under-optimized string concatenation (mistake #39): using strings.Builder instead of the + operator to concatenate strings.
+//2. Useless string conversions (mistake #40): whenever possible, avoid having to convert []byte into strings.
+//3. Inefficient slice and map initialization (mistakes #21 and #27): preallocate slices and maps if the length is already known.
+//4. Better data struct alignment to reduce struct size (mistake #94).
+//5. If we frequently allocate many objects of the same type, we can consider using sync.Pool
